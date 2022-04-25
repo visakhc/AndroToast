2055f59a3b97a89ad57c207d518f34e8d1535d83
VisakhC <visakh@meridian.net.in>
2022-04-25 21:26:58 +0530

    moved start() from AndroToast class to companion object

--- AndroToast/src/main/java/com/github/androtoast/AndroToast.kt
+++ AndroToast/src/main/java/com/github/androtoast/AndroToast.kt
@@ -1,3 +1,9 @@
+/*
+ * *
+ *  * Created by visakhc on 25-04-2022
+ *
+ */
+
 package com.github.androtoast
 
 import android.content.Context
@@ -16,15 +22,10 @@ import coil.loadAny
 import com.google.android.material.card.MaterialCardView
 
 
-class AndroToast(private val context: Context) {
-
-    init {
-        baseToast = Toast.makeText(context, "", duration)
-    }
+class AndroToast {
 
 //    todo add lottie
 //    todo add option to set custom size for imageview
-//    todo make enum classes
 
     companion object {
         const val SHORT = Toast.LENGTH_SHORT
@@ -49,123 +50,144 @@ class AndroToast(private val context: Context) {
         var yOffset: Int? = null
         var elevation: Float? = null
         var typeface: Int? = null
-        var cornerRadius: Float = 100f
+        var cornerRadius: Float = 30f
         var shadowColor: Int? = null
         var strokeColor: Int? = null
         var strokeWidth: Int = 0
         var textAnimation: Int? = null
-    }
 
 
-    fun start() {
-        val defGravity = Toast(context).gravity
-        val defX = Toast(context).xOffset
-        val defY = Toast(context).yOffset
+        fun start(context: Context) {
+            val defGravity = Toast(context).gravity
+            val defX = Toast(context).xOffset
+            val defY = Toast(context).yOffset
 
-        val Toasty = Toast.makeText(context, "", duration)
+            val toast = Toast.makeText(context, "", duration)
 
-        val toastLayout: View =
-            (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
-                .inflate(layout, null)
-        toastLayout.background.setTint(ContextCompat.getColor(context, tint))
+            val toastLayout: View =
+                (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                    .inflate(layout, null)
+            toastLayout.background.setTint(ContextCompat.getColor(context, tint))
 
-        val rootView = toastLayout.findViewById<MaterialCardView>(R.id.toast_root)
-        elevation?.let {
-            rootView?.cardElevation = it
-        }
-        rootView?.radius = cornerRadius
-        rootView?.strokeWidth = strokeWidth
+            takeIf { layout == R.layout.androtoast }?.apply {
 
-        strokeColor?.let {
-            val colorInt: Int = context.resources.getColor(it)
-            val csl = ColorStateList.valueOf(colorInt)
-            rootView?.setStrokeColor(csl)
-        }
+                val rootView = toastLayout.findViewById<MaterialCardView>(R.id.toast_root)
+                elevation?.let {
+                    rootView?.cardElevation = it
+                }
+                rootView?.radius = cornerRadius
+                rootView?.strokeWidth = strokeWidth
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            shadowColor?.let {
-                rootView?.setOutlineSpotShadowColor(ContextCompat.getColor(context, it))
+                strokeColor?.let {
+                    val colorInt: Int = context.resources.getColor(it)
+                    val csl = ColorStateList.valueOf(colorInt)
+                    rootView?.setStrokeColor(csl)
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    shadowColor?.let {
+                        rootView?.setOutlineSpotShadowColor(ContextCompat.getColor(context, it))
+                    }
+                }
+
+
+                val iconLeft = toastLayout.findViewById<ImageView>(R.id.iv_icon_left)
+                val iconRight = toastLayout.findViewById<ImageView>(R.id.iv_icon_right)
+                val iconTop = toastLayout.findViewById<ImageView>(R.id.iv_icon_top)
+                val iconBottom = toastLayout.findViewById<ImageView>(R.id.iv_icon_bottom)
+
+                if (drawableLeft != null) {
+                    iconLeft.loadAny(drawableLeft)
+                    drawableLeftTint?.let {
+                        iconLeft.setColorFilter(
+                            ContextCompat.getColor(
+                                context,
+                                it
+                            )
+                        )
+                    }
+                    iconLeft.show()
+                } else {
+                    iconLeft.hide()
+                }
+                if (drawableRight != null) {
+                    iconRight.loadAny(drawableRight)
+                    drawableRightTint?.let {
+                        iconRight.setColorFilter(
+                            ContextCompat.getColor(
+                                context,
+                                it
+                            )
+                        )
+                    }
+                    iconRight.show()
+                } else {
+                    iconRight.hide()
+                }
+                if (drawableTop != null) {
+                    iconTop.loadAny(drawableTop)
+                    drawableTopTint?.let {
+                        iconTop.setColorFilter(
+                            ContextCompat.getColor(
+                                context,
+                                it
+                            )
+                        )
+                    }
+                    iconTop.show()
+                } else {
+                    iconTop.hide()
+                }
+                if (drawableBottom != null) {
+                    iconBottom.loadAny(drawableBottom)
+                    drawableBottomTint?.let {
+                        println("here")
+                        iconBottom.setColorFilter(ContextCompat.getColor(context, it))
+                    }
+                    iconBottom.show()
+                } else {
+                    iconBottom.hide()
+                }
+
+                val messageTextView = toastLayout.findViewById<TextView>(R.id.tv_message)
+                messageTextView.setTextColor(ContextCompat.getColor(context, textColor))
+                messageTextView.text = text
+                messageTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
+                textAnimation?.let {
+                    messageTextView.startAnimation(AnimationUtils.loadAnimation(context, it))
+                }
+                if (typeface != null) {
+                    typeface?.let {
+                        val typeface = ResourcesCompat.getFont(context, it)
+                        messageTextView.typeface = typeface
+                    }
+                } else {
+                    messageTextView.typeface = null
+                }
             }
-        }
+
+            toast.view = toastLayout
 
 
-        val iconLeft = toastLayout.findViewById<ImageView>(R.id.iv_icon_left)
-        val iconRight = toastLayout.findViewById<ImageView>(R.id.iv_icon_right)
-        val iconTop = toastLayout.findViewById<ImageView>(R.id.iv_icon_top)
-        val iconBottom = toastLayout.findViewById<ImageView>(R.id.iv_icon_bottom)
-
-        if (drawableLeft != null) {
-            iconLeft.loadAny(drawableLeft)
-            drawableLeftTint?.let { iconLeft.setColorFilter(ContextCompat.getColor(context, it)) }
-            iconLeft.show()
-        } else {
-            iconLeft.hide()
-        }
-        if (drawableRight != null) {
-            iconRight.loadAny(drawableRight)
-            drawableRightTint?.let { iconRight.setColorFilter(ContextCompat.getColor(context, it)) }
-            iconRight.show()
-        } else {
-            iconRight.hide()
-        }
-        if (drawableTop != null) {
-            iconTop.loadAny(drawableTop)
-            drawableTopTint?.let { iconTop.setColorFilter(ContextCompat.getColor(context, it)) }
-            iconTop.show()
-        } else {
-            iconTop.hide()
-        }
-        if (drawableBottom != null) {
-            iconBottom.loadAny(drawableBottom)
-            drawableBottomTint?.let {
-                println("here")
-                iconBottom.setColorFilter(ContextCompat.getColor(context, it))
+            if (gravity == null && xOffset == null && yOffset == null) {
+                toast.setGravity(defGravity, defX, defY)
+            } else if (gravity == null && xOffset == null && yOffset != null) {
+                toast.setGravity(defGravity, defX, yOffset!!)
+            } else if (gravity == null && xOffset != null && yOffset == null) {
+                toast.setGravity(defGravity, xOffset!!, defY)
+            } else if (gravity == null && xOffset != null && yOffset != null) {
+                toast.setGravity(defGravity, defX, yOffset!!)
+            } else if (gravity != null && xOffset == null && yOffset == null) {
+                toast.setGravity(gravity!!, defX, defY)
+            } else if (gravity != null && xOffset == null && yOffset != null) {
+                toast.setGravity(gravity!!, defX, yOffset!!)
+            } else if (gravity != null && xOffset != null && yOffset == null) {
+                toast.setGravity(gravity!!, xOffset!!, defY)
+            } else if (gravity != null && xOffset != null && yOffset != null) {
+                toast.setGravity(gravity!!, xOffset!!, yOffset!!)
             }
-            iconBottom.show()
-        } else {
-            iconBottom.hide()
+
+            toast.show()
         }
-
-        val messageTextView = toastLayout.findViewById<TextView>(R.id.tv_message)
-        messageTextView.setTextColor(ContextCompat.getColor(context, textColor))
-        messageTextView.text = text
-        messageTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
-        textAnimation?.let {
-            messageTextView.startAnimation(AnimationUtils.loadAnimation(context, it))
-        }
-        if (typeface != null) {
-            typeface?.let {
-                val typeface = ResourcesCompat.getFont(context, it)
-                messageTextView.typeface = typeface
-            }
-        } else {
-            messageTextView.typeface = null
-        }
-
-
-
-        Toasty.view = toastLayout
-
-
-        if (gravity == null && xOffset == null && yOffset == null) {
-            Toasty.setGravity(defGravity, defX, defY)
-        } else if (gravity == null && xOffset == null && yOffset != null) {
-            Toasty.setGravity(defGravity, defX, yOffset!!)
-        } else if (gravity == null && xOffset != null && yOffset == null) {
-            Toasty.setGravity(defGravity, xOffset!!, defY)
-        } else if (gravity == null && xOffset != null && yOffset != null) {
-            Toasty.setGravity(defGravity, defX, yOffset!!)
-        } else if (gravity != null && xOffset == null && yOffset == null) {
-            Toasty.setGravity(gravity!!, defX, defY)
-        } else if (gravity != null && xOffset == null && yOffset != null) {
-            Toasty.setGravity(gravity!!, defX, yOffset!!)
-        } else if (gravity != null && xOffset != null && yOffset == null) {
-            Toasty.setGravity(gravity!!, xOffset!!, defY)
-        } else if (gravity != null && xOffset != null && yOffset != null) {
-            Toasty.setGravity(gravity!!, xOffset!!, yOffset!!)
-        }
-
-
-        Toasty.show()
     }
 }
--- app/src/main/java/com/github/vk/MainActivity.kt
+++ app/src/main/java/com/github/vk/MainActivity.kt
@@ -1,28 +1,15 @@
+/*
+ * *
+ *  * Created by visakhc on 25-04-2022
+ *
+ */
+
 package com.github.vk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import android.view.Gravity
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.github.androtoast.AndroToast
-import com.github.androtoast.AndroToast.Companion.drawableBottom
-import com.github.androtoast.AndroToast.Companion.drawableBottomTint
-import com.github.androtoast.AndroToast.Companion.drawableLeft
-import com.github.androtoast.AndroToast.Companion.drawableLeftTint
-import com.github.androtoast.AndroToast.Companion.drawableRight
-import com.github.androtoast.AndroToast.Companion.drawableRightTint
-import com.github.androtoast.AndroToast.Companion.drawableTop
-import com.github.androtoast.AndroToast.Companion.drawableTopTint
-import com.github.androtoast.AndroToast.Companion.elevation
-import com.github.androtoast.AndroToast.Companion.gravity
-import com.github.androtoast.AndroToast.Companion.shadowColor
-import com.github.androtoast.AndroToast.Companion.strokeColor
-import com.github.androtoast.AndroToast.Companion.strokeWidth
-import com.github.androtoast.AndroToast.Companion.text
-import com.github.androtoast.AndroToast.Companion.textAnimation
-import com.github.androtoast.AndroToast.Companion.textColor
-import com.github.androtoast.AndroToast.Companion.tint
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,29 +18,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btClick).setOnClickListener {
-            AndroToast(this@MainActivity).apply {
-                text = "Hi I'm Testing"
-//                textColor = R.color.white
-//                textAnimation = R.anim.wobble
 
-//                tint = R.color.red
-//                gravity = Gravity.CENTER
-//                strokeColor = R.color.green
-//                strokeWidth = 4
-//                elevation = 20f
-//                shadowColor = R.color.red
+            AndroToast.apply {
+                textColor = R.color.yellow
+                text = "Hi I'm Testing"
+                // textColor = R.color.white
+                // textAnimation = R.anim.wobble
+
+                // tint = R.color.red
+                // gravity = Gravity.CENTER
+                // strokeColor = R.color.green
+                // strokeWidth = 4
+                // elevation = 20f
+                // shadowColor = R.color.red
 
                 drawableLeft = R.drawable.ic_android_black_24dp
-//                drawableRight = R.drawable.ic_android_black_24dp
-//                drawableTop = R.drawable.ic_android_black_24dp
-//                drawableBottom = R.drawable.ic_android_black_24dp
+                // drawableRight = R.drawable.ic_android_black_24dp
+                // drawableTop = R.drawable.ic_android_black_24dp
+                // drawableBottom = R.drawable.ic_android_black_24dp
 
-//                drawableLeftTint = R.color.green
-//                drawableRightTint = R.color.yellow
-//                drawableTopTint = R.color.white
-//                drawableBottomTint = R.color.black
+                // drawableLeftTint = R.color.green
+                // drawableRightTint = R.color.yellow
+                // drawableTopTint = R.color.white
+                // drawableBottomTint = R.color.black
 
-            }.start()
+            }.start(this)
+
         }
     }
 }
